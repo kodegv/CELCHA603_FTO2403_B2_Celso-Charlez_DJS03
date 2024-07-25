b13ec9726722ad81b138e19d6b60d23f0718d56f
kodegv <celsocharlez1@gmail.com>
2024-07-25 12:13:36 +0200

    dom call and fetch, added create element

--- scripts.js
+++ scripts.js
@@ -34,3 +34,45 @@ const htmlElements = {
   dataListActive: document.querySelector("[data-list-active]"),
   dataListClose: document.querySelector("[data-list-close]"),
 };
+
+// Calls functions on dom load
+document.addEventListener("DOMContentLoaded", () => {
+  getMETAHTML();
+  addBookPreview();
+  setupEventListeners();
+});
+
+//Fetches data then adds it to dom
+function getMETAHTML() {
+  fetch("./meta.html")
+    .then((response) => response.text())
+    .then((data) => {
+      htmlElements.head.innerHTML = data;
+    });
+}
+
+// Creates book element for html
+function createBookElement({ author, id, image, title }) {
+  const element = document.createElement("button");
+  element.classList.add("preview");
+  element.dataset.preview = id;
+
+  element.innerHTML = `
+    <img class="preview__image" src="${image}" />
+    <div class="preview__info">
+      <h3 class="preview__title">${title}</h3>
+      <div class="preview__author">${authors[author]}</div>
+    </div>
+  `;
+
+  return element;
+}
+// Adds book preview to html
+function addBookPreview() {
+  const starting = document.createDocumentFragment();
+  for (const book of matches.slice(0, BOOKS_PER_PAGE)) {
+    const element = createBookElement(book);
+    starting.appendChild(element);
+  }
+  htmlElements.dataListItems.appendChild(starting);
+}
